b2b3dc928dbe4aacf930d7828634a843aa89722b
timaenho <tom.maenhout@gmail.com>
2022-05-19 17:51:32 -0300

    location/coordinatos home

--- app/src/main/java/ort/tp3_login/fragments/Home_turista.kt
+++ app/src/main/java/ort/tp3_login/fragments/Home_turista.kt
@@ -1,16 +1,25 @@
 package ort.tp3_login.fragments
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
 import ort.tp3_login.R
 import ort.tp3_login.adapters.ServicioAdapter
@@ -20,17 +29,27 @@ import ort.tp3_login.viewModels.ViewModelHomeTurista
 
 class home_turista : Fragment() {
 
-
+    //Vista
     lateinit var view1 : View
 
+    //ViewModel
     private lateinit var viewModel: ViewModelHomeTurista
+
+    //RecyclerView
     lateinit var recyclerView: RecyclerView
     var cardsTuristaLista : MutableList<ServicioCard> = ArrayList<ServicioCard>()
     private lateinit var linearLayoutManager: LinearLayoutManager
     lateinit var adapter: ServicioAdapter
 
+    //Buscar
     lateinit var buscareditText: EditText
     lateinit var buscarButton: Button
+
+
+    //Locación
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    var myLongitude : Double = 0.0
+    var myLatitude : Double = 0.0
 
 
     override fun onCreateView(
@@ -44,15 +63,38 @@ class home_turista : Fragment() {
         //buscarButton = view1.findViewById(R.id.buscarButton)
         recyclerView = view1.findViewById(R.id.recyclerViewHomeTurista)
 
+
         return view1
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(view1.context)
+        setmylocation ()
         viewModel = ViewModelProvider(requireActivity()).get(ViewModelHomeTurista::class.java)
         viewModel.lista.observe(viewLifecycleOwner, Observer{result ->
             cardsTuristaLista = result
         })
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setmylocation() {
+        if(ContextCompat.checkSelfPermission(view1.context,android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED){
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    myLatitude = location!!.longitude
+                    myLongitude = location!!.latitude
+                    Log.d("longitude",myLongitude.toString())
+                    Log.d("latitude",myLatitude.toString())
+                }
+        }else{
+            ActivityCompat.requestPermissions(
+                view1.context as Activity,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
     }
 
 
