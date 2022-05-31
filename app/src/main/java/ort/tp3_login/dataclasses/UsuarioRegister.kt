2ff69e01d3c99af6b487006ec3223616e5703b2f
alejandrogrosso <alematiasgrosso@gmail.com>
2022-05-31 15:32:31 -0300

    Create UsuarioRegister.kt

---
+++ app/src/main/java/ort/tp3_login/dataclasses/UsuarioRegister.kt
@@ -0,0 +1,12 @@
+package ort.tp3_login.dataclasses
+
+
+
+data class UsuarioRegister(
+    var username: String,
+    var firstName: String,
+    var lastName: String,
+    var email: String,
+    var password: String,
+    var isGuide: Boolean
+)
