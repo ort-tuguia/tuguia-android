78832a5d148798f3473c9915c2f43e6eac91aa24
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-03 18:25:55 -0300

    Update Home_turista.kt

--- app/src/main/java/ort/tp3_login/fragments/Home_turista.kt
+++ app/src/main/java/ort/tp3_login/fragments/Home_turista.kt
@@ -1,12 +1,12 @@
 package ort.tp3_login.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -16,13 +16,12 @@ import androidx.lifecycle.liveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_login.*
+import com.google.gson.Gson
 import ort.tp3_login.R
 import ort.tp3_login.adapters.ServicioAdapter
 import ort.tp3_login.dataclasses.CategoriaItem
 import ort.tp3_login.dataclasses.Categorias
 import ort.tp3_login.dataclasses.ServicioService
-import ort.tp3_login.dataclasses.Servicios
 import ort.tp3_login.entities.ServicioCard
 import ort.tp3_login.services.RetrofitInstance
 import ort.tp3_login.viewModels.ViewModelHomeTurista
@@ -33,7 +32,7 @@ class home_turista : Fragment() {
 
     //Vista
     lateinit var view1 : View
-
+    var gson : Gson = Gson()
     //ViewModel
     private  val viewModel: ViewModelHomeTurista by activityViewModels()
 
@@ -49,6 +48,8 @@ class home_turista : Fragment() {
     lateinit var selectedCategorie: BooleanArray
     //lateinit var categorieListInt : ArrayList<Int>
     lateinit var resultCategorie: MutableListIterator<CategoriaItem>
+
+    val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
 
     private fun fetchCategories(){
         val retService : ServicioService = RetrofitInstance
@@ -122,7 +123,7 @@ class home_turista : Fragment() {
 
     override fun onStart() {
         super.onStart()
-
+        Log.d("usuario en Home turista", viewModel.user.value.toString())
 
     }
 
