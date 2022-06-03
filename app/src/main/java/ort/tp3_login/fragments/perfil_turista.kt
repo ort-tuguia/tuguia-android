e0f4f427d7c7ff48ece330a9ddd352ee2b336717
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-03 20:14:32 -0300

    Update perfil_turista.kt

--- app/src/main/java/ort/tp3_login/fragments/perfil_turista.kt
+++ app/src/main/java/ort/tp3_login/fragments/perfil_turista.kt
@@ -12,21 +12,15 @@ import ort.tp3_login.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [perfil_turista.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class perfil_turista : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -35,26 +29,9 @@ class perfil_turista : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_perfil_turista, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment perfil_turista.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            perfil_turista().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
