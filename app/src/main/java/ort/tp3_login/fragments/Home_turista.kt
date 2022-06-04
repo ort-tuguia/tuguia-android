27cd7c48305ce888fd2fdcf0d2c1d8a26306e5b9
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-04 20:27:30 -0300

    Update Home_turista.kt

--- app/src/main/java/ort/tp3_login/fragments/Home_turista.kt
+++ app/src/main/java/ort/tp3_login/fragments/Home_turista.kt
@@ -1,11 +1,14 @@
 package ort.tp3_login.fragments
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -17,6 +20,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_home_turista.*
 import ort.tp3_login.R
 import ort.tp3_login.adapters.ServicioAdapter
 import ort.tp3_login.dataclasses.CategoriaItem
@@ -45,11 +49,14 @@ class home_turista : Fragment() {
     //Buscar
     lateinit var TextViewCategories: TextView
     lateinit var buscarButton: Button
-    lateinit var selectedCategorie: BooleanArray
-    //lateinit var categorieListInt : ArrayList<Int>
+     var selectedCategorie: ArrayList<Boolean> = ArrayList<Boolean>()
+     var categorieListInt : ArrayList<Int> = ArrayList<Int>()
     lateinit var resultCategorie: MutableListIterator<CategoriaItem>
+    var categoriesNombre : ArrayList<String> = ArrayList<String>()
+    lateinit var dialog : AlertDialog
 
-
+    var categoriesParaBackend : ArrayList<String> = ArrayList<String>()
+    var categoriesAux : MutableMap<String,CategoriaItem> = HashMap()
 
 
     private fun fetchCategories(){
@@ -58,18 +65,21 @@ class home_turista : Fragment() {
             .create(ServicioService::class.java)
         val responseLiveData : LiveData<Response<Categorias>> = liveData{
             val response = retService.getCategories()
-            Log.d("response", response.toString())
+            Log.d("Categorias", response.body().toString())
             emit(response)
         }
         responseLiveData.observe(viewLifecycleOwner,Observer{
             val categoriasList = it.body()?.listIterator()
             if (categoriasList != null) {
-                Log.d("categoriasList", categoriasList.toString())
+                //Log.d("categoriasList", categoriasList.toString())
                 resultCategorie = categoriasList
+
+
             }else{
                 Log.d("categoriasList","es null")
             }
         })
+
     }
 
 
@@ -82,13 +92,14 @@ class home_turista : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         view1 = inflater.inflate(R.layout.fragment_home_turista, container, false)
-        TextViewCategories = view1.findViewById(R.id.TextViewCategories)
+        TextViewCategories = view1.findViewById(R.id.textViewCategories)
         //buscarButton = view1.findViewById(R.id.buscarButton)
         recyclerView = view1.findViewById(R.id.recyclerViewHomeTurista)
-
         fetchCategories()
-        //selectedCategorie = BooleanArray(categorieListInt.size)
 
+        TextViewCategories.setOnClickListener {
+            createDialog()
+        }
 
 
 
@@ -132,6 +143,74 @@ class home_turista : Fragment() {
     private fun onItemClick(position: Int) : Boolean {
         Snackbar.make(view1,"vamos a detalle de $position", Snackbar.LENGTH_SHORT).show()
         return true
+    }
+
+    private fun createDialog(){
+        val builder = AlertDialog.Builder(context)
+        resultCategorie.forEach { categoria ->
+            categoriesNombre.add(categoria.name)
+            categoriesAux[categoria.name]= categoria
+        }
+        if (selectedCategorie.isEmpty()){
+            repeat(categoriesNombre.count()) {selectedCategorie.add(false)}
+        }
+        val categoriasArray: Array<String> = categoriesNombre.toTypedArray()
+
+
+        builder.setTitle("Seleccione categorias")
+        builder.setMultiChoiceItems(
+            categoriasArray,
+            selectedCategorie.toBooleanArray()
+        ) { dialog, which, isChecked ->}
+        builder.setPositiveButton("Submit"){dialog,which->
+            val alertDialog = dialog as AlertDialog
+            val sparseBooleanArray = alertDialog.listView.checkedItemPositions
+            var counter = 0
+
+            textViewSelect.text = ""
+            categoriasArray.forEachIndexed { index, s ->
+                if (sparseBooleanArray.get(index, false)) {
+                    categoriesAux[s]?.let { categoriesParaBackend.add(it.id) }
+                    Log.d("Categoria --> IF ID" , categoriesParaBackend[counter].toString())
+
+                    //textViewSelect.append("\n$s")
+                    counter += 1
+                }
+            }
+
+            if (counter > 0) {
+
+
+           }
+        }
+            builder.setNeutralButton("Cancel"){dialog,which->
+                textViewSelect.text = ""
+            }
+            builder.setCancelable(false)
+
+        val dialog = builder.create()
+        dialog.show()
+
+        if((selectedCategorie.filter { it }).size < 2){
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+        }
+        dialog.listView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val sparseBooleanArray = dialog.listView.checkedItemPositions
+                var checkedItems = 0
+                categoriasArray.forEachIndexed { index, s ->
+                    if (sparseBooleanArray.get(index,false)){
+                        checkedItems +=1
+                        selectedCategorie[index] = true
+                    }else{
+                        selectedCategorie[index] = false
+                    }
+                }
+
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                    .isEnabled = checkedItems >=2
+            }
+
     }
 }
 
