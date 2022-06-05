164121ba495f1ed7671b55f198a878ff02b5b543
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-05 18:39:18 -0300

    Update ServicioService.kt

--- app/src/main/java/ort/tp3_login/dataclasses/ServicioService.kt
+++ app/src/main/java/ort/tp3_login/dataclasses/ServicioService.kt
@@ -1,29 +1,18 @@
 package ort.tp3_login.dataclasses
 
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ServicioService {
     @GET("activities")
-    suspend fun getServicios() : Response<Servicios>
+    suspend fun getServicios(@Header("Authorization") token: String) : Response<Servicios>
 
-    @GET("activities/search")
-    suspend fun searchServicios(
-             @Query("currentLatitude"
-        )currentLatitude:Double,
-             @Query("currentLongitude"
-        )currentLongitude:Double,
-            @Query("maxKm"
-        )maxKm :Double,
-             @Query("maxResults"
-        )maxResults :Int) : Response<Servicios>
+    @POST("activities/search")
+    suspend fun searchServicios(@Body serviciosSearch: ServiciosSearch , @Header("Authorization") token: String) : Response<Servicios>
 
 
     @GET("categories")
-    suspend fun getCategories(): Response<Categorias>
+    suspend fun getCategories( @Header("Authorization") token: String): Response<Categorias>
 
     @POST("users/login")
     suspend fun getLogin(@Body login: Login)
