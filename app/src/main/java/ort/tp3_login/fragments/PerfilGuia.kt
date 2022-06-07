06a2584169769d161292ef39aa8c289973e360ba
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-07 17:34:39 -0300

    Add Pick Image to Guia

--- app/src/main/java/ort/tp3_login/fragments/PerfilGuia.kt
+++ app/src/main/java/ort/tp3_login/fragments/PerfilGuia.kt
@@ -1,17 +1,26 @@
 package ort.tp3_login.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
+import de.hdodenhof.circleimageview.CircleImageView
 import ort.tp3_login.R
 import ort.tp3_login.viewModels.ViewModelHomeTurista
 
 
 class PerfilGuia : Fragment() {
+
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
+    }
+    lateinit var circleImageView: CircleImageView
     lateinit var v: View
     private  val viewModel: ViewModelHomeTurista by activityViewModels()
 
@@ -26,11 +35,36 @@ class PerfilGuia : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        v=inflater.inflate(R.layout.fragment_perfil_turista, container, false)
+        v=inflater.inflate(R.layout.fragment_perfil_guia, container, false)
         v.findViewById<TextView>(R.id.user_name).text = viewModel.user.value?.firstName + " " + viewModel.user.value?.lastName
         v.findViewById<TextView>(R.id.user_email).text = viewModel.user.value?.email
+        circleImageView = v.findViewById(R.id.circleImageViewGuia)
         return  v
     }
+    override fun onStart() {
+        super.onStart()
+
+        circleImageView.setOnClickListener{
+            pickImageGallery()
+        }
+    }
+
+    private fun pickImageGallery () {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        getResult.launch(intent)
+    }
+
+
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK){
+                val value = it.data?.getStringExtra("input")
+                circleImageView.setImageURI(it.data?.data)
+                //circleImageView.setImageURI(value)
+            }
+        }
 
 
 }
