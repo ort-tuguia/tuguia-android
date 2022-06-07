14605f859f27f0f65813d1f7a648f38a991f31a3
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-07 17:34:21 -0300

    Add Pick Image to Turista

--- app/src/main/java/ort/tp3_login/fragments/perfil_turista.kt
+++ app/src/main/java/ort/tp3_login/fragments/perfil_turista.kt
@@ -1,17 +1,29 @@
 package ort.tp3_login.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
+import de.hdodenhof.circleimageview.CircleImageView
 import ort.tp3_login.R
 import ort.tp3_login.viewModels.ViewModelHomeTurista
 
 
+
+
 class perfil_turista : Fragment() {
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
+
+
+    }
+    lateinit var circleImageView: CircleImageView
     lateinit var v: View
     private  val viewModel: ViewModelHomeTurista by activityViewModels()
 
@@ -29,8 +41,37 @@ class perfil_turista : Fragment() {
         v=inflater.inflate(R.layout.fragment_perfil_turista, container, false)
         v.findViewById<TextView>(R.id.user_name).text = viewModel.user.value?.firstName + " " + viewModel.user.value?.lastName
         v.findViewById<TextView>(R.id.user_email).text = viewModel.user.value?.email
+        circleImageView = v.findViewById(R.id.circleImageViewTurista)
+
+
+
         return  v
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        circleImageView.setOnClickListener{
+            pickImageGallery()
+        }
+    }
+
+    private fun pickImageGallery () {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        getResult.launch(intent)
+    }
+
+
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK){
+                val value = it.data?.getStringExtra("input")
+                circleImageView.setImageURI(it.data?.data)
+                //circleImageView.setImageURI(value)
+            }
+        }
 
 
 }
