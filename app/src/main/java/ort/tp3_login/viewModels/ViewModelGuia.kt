7df4f64ed83b937e40daab2ee28f2d32da77567b
Tom Maenhout <tom.maenhout@gmail.com>
2022-06-11 13:02:13 -0300

    agregar_actividad

--- app/src/main/java/ort/tp3_login/viewModels/ViewModelGuia.kt
+++ app/src/main/java/ort/tp3_login/viewModels/ViewModelGuia.kt
@@ -1,6 +1,7 @@
 package ort.tp3_login.viewModels
 
 import android.location.Location
+import android.net.Uri
 import android.widget.EditText
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,13 +11,16 @@ class ViewModelGuia: ViewModel() {
     var user = MutableLiveData<UsuarioLogin>()
     var token :String = ""
 
+
     // agregarServicio
-    var servicioName: String = ""
-    var servicioDescription: String = ""
+    //var servicioName: String = ""
+    var servicioCategoriaId : String = ""
+    //var servicioDescription: String = ""
     var servicioLocationlat: Double = 0.0
     var servicioLocationlon: Double = 0.0
-    var servicioPrice: Double= 0.0
-    var servicioUrlFoto: Double=0.0
+    //var servicioPrice: Double= 0.0
+    var servicioUrlFoto: Uri? = null
+    var  categorias: Array<CharSequence>? = null
 
 
 
