165bd7956899b3d3a149d3e35717e24e514d7a7a
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-12 19:26:16 -0300

    Update ServicioCard.kt

--- app/src/main/java/ort/tp3_login/entities/ServicioCard.kt
+++ app/src/main/java/ort/tp3_login/entities/ServicioCard.kt
@@ -1,27 +1,26 @@
 package ort.tp3_login.entities
 
+import android.net.Uri
+
 class ServicioCard
                     (nombreGuia : String?,
-
                     titulo : String?,
                     profilePic: Int?,
                     valoracion: Int?,
-                    imagen: Int?,
+                    imagen: Uri?,
                     categoria: String?
 ) {
 
     var nombreGuia : String
-
     var titulo : String
     var profilePic : Int
     var valoracion : Int
-    var imagen : Int
+    var imagen : Uri
     var categoria : String
 
 
     init {
         this.nombreGuia = nombreGuia!!
-
         this.titulo = titulo!!
         this.profilePic = profilePic!!
         this.valoracion = valoracion!!
